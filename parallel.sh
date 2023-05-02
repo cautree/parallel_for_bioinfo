07f0bcc079b559e420785d3f3bb4377e1b0732d8
Yanyan Liu <yanyanliujs@gmail.com>
2023-05-02 14:40:08 -0400

    Update parallel.sh

--- parallel.sh
+++ parallel.sh
@@ -7,3 +7,6 @@ parallel --dry-run -j 4 -k echo \"{} "<-- a number"\" ::: `seq 1 5`
 #4
 $5
 cat seq | parallel --dry-run -j 4 -k echo \"{} "<-- a number"\"
+
+##real example, get the coverage
+cat file|sed  s/\ //g | parallel "samtools depth bam/{}.bam > bam_coverage/{}.coverage"
