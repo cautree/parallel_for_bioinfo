4ffce63a62dea112b21ab18e0be5158a69c788e8
Yanyan Liu <yanyanliujs@gmail.com>
2023-05-02 14:47:16 -0400

    Create useful_bash.sh

---
+++ useful_bash.sh
@@ -0,0 +1,19 @@
+
+## this is for batch download stuff from s3 bucket, first create a temp file, then loop the temp file
+
+aws s3 ls s3://s3_bucket/ | grep ecoli_REL606 | grep -v _ecoli_ |\
+    awk '{$1=$1}1' OFS="," |\
+    cut -f 4 -d "," > temp
+
+
+#!/bin/bash
+set -e
+while read line;
+do
+  aws s3 cp s3://s3_bucket/$line  ref/
+done <temp
+
+
+
+
+
