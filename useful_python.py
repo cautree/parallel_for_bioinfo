cbf0f788ea681cd5945e1d5f8d0e0a452bc7d284
Yanyan Liu <yanyanliujs@gmail.com>
2023-05-02 15:44:54 -0400

    update python file

--- useful_python.py
+++ useful_python.py
@@ -1,1 +1,22 @@
+import pandas as pd
+df = pd.read_csv('datasets/all_stocks_2017-2018.csv')
 
+## check the columns names
+df.columns
+
+## make a Series into a dataframe
+df.Name.value_counts().to_frame().reset_index()
+
+## filter based on a column value, then reset the index
+df_tech = df[df.Name.isin(['AAPL','GOOGL','MSFT','AMZN'])].reset_index()
+
+#Pivot dataframe, so that each stock is a column, more like tidyr::spread in R
+#only showing the info for Close, not Open, High Low
+pivoted_df = df_tech.pivot(index='Date', columns='Name', values='Close')
+
+# Repeat the shifting and calculation for the 14, 21, 28-day offset
+# calculate returns over each window (offset) and store them in a dictionary with a loop
+# a smart way, and create the dict name on the go
+delta_dict = {}
+for offset in [7, 14, 21, 28]:
+    delta_dict['delta_{}'.format(offset)] = pivoted_df / pivoted_df.shift(offset) - 1.0
