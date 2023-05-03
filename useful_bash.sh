a8962a120b809be8449be12e7fb556ea8301dbe3
Yanyan Liu <yanyanliujs@gmail.com>
2023-05-03 10:41:41 -0400

    Update useful_bash.sh

--- useful_bash.sh
+++ useful_bash.sh
@@ -17,7 +17,9 @@ done <temp
 ## regex for well location of 96 well plate
 cat info_email | tail -10 | grep -o -E  "[A-H]{1}[0-9]{2}" > well.txt
 
-
+## show the files that match the patten in a file, then move that file to a fold called keep
+ls -1 | grep -f ../../meta_data/20230322_20ng.txt > file
+cat file | parallel --eta --verbose "mv {} keep"
 
 
 
