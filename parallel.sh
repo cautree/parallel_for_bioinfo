8203b78ac0f14a7b71b3491e64365db9889090c7
Yanyan Liu <yanyanliujs@gmail.com>
2023-05-05 15:29:48 -0400

    Update parallel.sh

--- parallel.sh
+++ parallel.sh
@@ -9,7 +9,7 @@ parallel --dry-run -j 4 -k echo \"{} "<-- a number"\" ::: `seq 1 5`
 cat seq | parallel --dry-run -j 4 -k echo \"{} "<-- a number"\"
 
 ##real example, get the coverage
-ls -1 bam/20230420_file/ | tr ".bam" " " > file
+ls -1 bam/20230420_file/ | sed 's/.bam//g' > file
 cat file|sed  s/\ //g | parallel "samtools depth -a bam/{}.bam > bam_coverage/{}.coverage"
 
 cat file | parallel -j 4 "bwa mem  ref/hg38.fa fastq/{}_R1_001.fastq.gz fastq/{}_R2_001.fastq.gz | samtools view -bh -F2048 - | samtools sort > bam/{}.bam"
