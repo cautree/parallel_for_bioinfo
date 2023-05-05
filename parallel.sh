77549ce48c58be8a910d9198b7f4a3d500ccfcb7
Yanyan Liu <yanyanliujs@gmail.com>
2023-05-05 14:34:22 -0400

    Update parallel.sh

--- parallel.sh
+++ parallel.sh
@@ -12,4 +12,6 @@ cat seq | parallel --dry-run -j 4 -k echo \"{} "<-- a number"\"
 ls -1 bam/20230420_file/ | tr ".bam" " " > file
 cat file|sed  s/\ //g | parallel "samtools depth -a bam/{}.bam > bam_coverage/{}.coverage"
 
+cat file | parallel -j 4 "bwa mem  ref/hg38.fa fastq/{}_R1_001.fastq.gz fastq/{}_R2_001.fastq.gz | samtools view -bh -F2048 - | samtools sort > bam/{}.bam"
+
 
