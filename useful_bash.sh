6a0bc410f532e464fd468fe487886264ed1e15e5
Yanyan Liu <yanyanliujs@gmail.com>
2023-05-22 12:32:13 -0400

    Update useful_bash.sh

--- useful_bash.sh
+++ useful_bash.sh
@@ -186,3 +186,131 @@ alias awk = "awk -F '\t' =v OFS='\t' "
 i() { (head -n 2; tail -n 2) < "$1" | column -t}
 i a.txt
 
+
+
+
+## from https://mp.weixin.qq.com/s?__biz=MzIxMjQxMDYxNA==&mid=2247484065&idx=1&sn=580783ce880008b2809b65d060460d6f&chksm=9747cb38a030422ecea3dcfaed25b7b7d15bf6119e66e919baeab5a601826eb3e5cb3312868a&token=1106388583&lang=zh_CN&scene=21#wechat_redirect
+## 作者：简佐义（jianzuoyi@qq.com）
+ls `pwd`/file
+cat R1.fq.gz R2.fq.gz  # by row
+paste -d ' ' file1 file2 #by column
+grep '^>' test.fa | cut -c 2-  ## -c is by character position
+less -SN file                # 显示文件的行号，并且截断太长的行
+tail -n +2 file	             # 跳过第1行，显示从第2行开始的所有行，可用于跳过文件的标题行
+touch {file1,file2,file3}	 # 同时创建3个文件
+tar czvf file.tar.gz files	 # 打包并压缩
+tar xvf file.tar.gz			 # 解包，解压缩
+gzip file					 # 压缩
+gunzip file.gz				 # 解压
+chmod a+x file	             # 增加[所有人]可执行权限
+chmod a-x file	             # 取消[所有人]可执行权限
+sort -k2,2 -k3,3 file	     # 先按第2列排序，第2列相同，再按第3列排序
+sort -k2,2n file		     # 按第2列排序，且第2列是数字，升序
+sort -k2,2nr file		     # 按第2列排序，且第2列是数字，降序
+sort -u file			     # 先排序文件，然后去除相邻的重复行，只保留一条记录
+sort file | uniq		     # 去除相信的重复行，只保留一条记录，相当于： sort -u file
+sort a b | uniq			     # 并集
+sort a b | uniq -d > c	     # 交集
+sort a c | uniq -u 		     # 补集
+df -h		                 # 查看磁盘使用情况，-h表示以人类可读的方式显示容量大小
+du -sh		                 # 查看当前目录使用了多少磁盘空间
+du -sh *	                 # 查看当前目录下各文件或文件夹使用的磁盘空间
+free -h		                 # 查看内存使用情况
+top -c		                 # 查看CPU，内存的使用情况
+ps aut		                 # 查看后台任务运行情况，第2列是任务的PID号
+kill -9 PID                  # 删除编号为PID的任务
+nohup ./run.sh &> run.sh.o & # 远程任务管理
+##########################################################################
+./run.sh	                 # 假如任务是直接这样开始跑的
+ctrl + z	                 # 按ctrl + z，将任务放到后台
+jobs                         # 输入jobs命令，回车，可以看到任务是暂停的： [1]+  Stopped(SIGTSTP)        bash run.sh
+bg                           # 让后台暂停的任务开始运行
+jobs                         # 再次运行jobs，可以看到任务已经跑起来了：   [1]+  Running                 bash run.sh &
+disown -r 	                 # 从当前shell中移除运行中的作业，至此，可以关掉终端回家了
+##########################################################################
+./run.sh > run.sh.o		     # 标准输出到run.sh.o日志文件
+./run.sh 2> run.sh.e	     # 标准错误输出到run.sh.e错误日志文件
+./run.sh &> run.sh.log	     # 标准输出和标准错误都输出到定一个文件
+./run.sh &> /dev/null	     # 丢弃标准输出和标准错误信息
+###################################################################
+command < file			     # 将file的内容作为command的输入
+command << END			     # 从标准输入（键盘）中读取数据，直到遇到分界符END时停止（分界符用户可以自定义）
+command <file1 > file2	     # 将file1作为command的输入，并将处理结果输出到file2
+### one application
+whileread line
+do
+    do something
+done < file.txt > result.txt
+##################################################################
+find -name file					# 在当前目录查找名为file的文件
+find dir/ -name file			# 在dir/目录下查找名为file的文件
+find dir/ -name '*file*'# 在dir/目录下查找包含file关键词的文件，-name参数支持正则表达式
+find dir/ -name file -delete	# 查找文件并删除
+
+#####################################################################
+cat file | xargs		                                            # 将file的内容显示成一行
+find /ifs/result -name '*.fq.gz' | xargs -n1 -I{} cp {} /ifs/data/	# 查找fq.gz文件并复制到/ifs/data目录下
+find /ifs/result -name '*.fq.gz' | xargs tar czvf all.fq.gz			# 查找fq.gz文件并打包在一起
+find . -type f -name '*.log' -print0 | xargs -0 rm -f				# 当rm文件过多时，可以这样删除
+find . -type f -name '*.py' -print0 | xargs -0 wc -l				# 统计一个目录中所有python文件的行数
+#####################################################################
+find *.fq | parallel -j 12 "fastqc {} --outdir ."# 同时执行12个Fastqc任务
+#####################################################################
+grep pattern files			                # 搜索文件中包含pattern的行
+grep -v pattern files		                # 搜索文件中不包含pattern的行
+grep -f pattern.txt files	                # 搜索的pattern来自于文件中
+grep -i pattern files		                # 不区分大小写。默认搜索是区分大小写的
+grep -w pattern files		                # 只匹配整个单词，而不是字符串的一部分（如搜索hello，不会匹配到helloworld）
+grep -n pattern files		                # 显示行号信息
+grep -c pattern files		                # 显示匹配的行数
+grep -l pattern files		                # 只显示匹配的文件名
+grep -L pattern files		                # 显示不匹配的文件名
+grep -C number pattern files                # 额外显示匹配行的上下[number]行
+grep pattern1 | grep pattern2 files         # 显示既匹配pattern1，又匹配pattern2的行
+grep -E "pattern1|pattern2" files	        # 显示匹配pattern1或者pattern2的行, grep -E相当于egrep
+
+# 用于搜索的特殊字符
+^: 表示行前
+$: 表示行尾
+
+grep '^#' result.vcf		                # 显示VCF文件的表头信息
+grep '^hello$' files		                # 显示只包含hello的行
+grep -v '^\s*$' file		                # 删除空白行
+
+########################################################################
+##sed
+d：删除该行
+p：打印该行
+i：在行的前面插入新行
+a：在行的后面插入新行
+r：读取指定文件的内容。
+w：写入指定文件。
+sed -n '10p' file		                # 显示第10行
+sed -n '10,20p' file	                # 显示第10到20之间的行
+sed -n '/pattern/p' file                # 显示含有pattern的行
+sed -n '/pattern1/,/pattern2/p' file    # 显示patter1与pattern2之间的行
+
+sed '10d' file			                # 删除第10行
+sed '10,20d' file		                # 删除第10到20之间的行
+sed '/pattern/d'                        # 删除匹配pattern的行
+sed '/^\s*$/d' file		                # 删除空白行
+sed 's/^\s*//' file		                # 删除行前的空白：空格，制表符
+sed 's/\s*$//' file		                # 删除行尾的空白：空格，制表符
+sed 's/^\s*//;s/\s*$//' file            # 删除行首和行尾的空白：空格，制表符
+
+sed 's/AA/BB/' file		                # 将文件中的AA替换成BB，只替换一行中第一次出现的AA，替换后的结果输出到屏幕
+sed 's/AA/BB/g' file	                # 将文件中的所有AA都替换成BB，替换后的结果输出到屏幕
+sed -i 's/AA/BB/g' file                 # 将文件中的所有AA都替换成BB，直接更改文件的内容
+sed '/CC/s/AA/BB/g' file                # 只替换那些含有CC的行
+sed 's/pattern/&XXXX/' file	            # 在pattern之后加上XXXX。&表示之前被匹配的内容
+sed 's/pattern.*/&XXXX' file            # 在匹配pattern的行尾加上XXXX。pattern.*表示包含pattern的整行内容
+
+sed -n '1~4s/^@/>/p;2~4p' file.fq > file.fa	# Fastq文件转Fasta文件
+sed -n '2~4p' file.fq		            # 提取Fastq文件的序列
+
+sed 'y/ABC/XYZ/' file	                # 将ABC逐字替换成XYZ
+
+sed '1i\hello' file		                # 在第1行前面插入一行，内容为hello，通常用来为文件增加标题
+sed '1a\hello' file		                # 在第1行后面插入一行，内容为hello
+sed '1r file2' file1	                # 在第1行后面读入file2的内容
+sed '/pattern/w file2' file1 # 将匹配的行写入file2中
