cad1ddd25450eabfb5675efaa8b74812a10ad4ab
Yanyan Liu <yanyanliujs@gmail.com>
2023-05-31 16:09:14 -0400

    Update useful_R.R

--- useful_R.R
+++ useful_R.R
@@ -79,3 +79,76 @@ animals <- str_extract_all(lookaround, "\\w+\\s(?=costs)")
 
 
 ##in R studio, if you highlight some text and do CMD-Option-Return on a Mac, the text gets sent to the RStudio Unix Terminal
+
+
+
+# run model
+race_model = function(df){
+  aov(formula = meta_reading ~ race_category, data = df)
+}
+
+gender_model = function(df){
+  aov(formula = meta_reading ~ gender, data = df)
+}
+df_mod_for_race = df %>% 
+  tidyr::gather( `EPA_0`: `MCTR2_8.8`, key = "meta", value = "meta_reading")  %>% 
+  tidyr::nest(-meta) %>% 
+  mutate( mod = purrr::map(.$data, race_model)) %>% 
+  mutate( mod_tidy= purrr::map(.$mod, broom::tidy)) %>% 
+  select( meta, mod_tidy) %>% 
+  tidyr::unnest() %>% 
+  filter( term =="race_category") %>% 
+  select(meta, p.value) %>% 
+  mutate( response = "race") %>% 
+  arrange(p.value)
+
+
+
+  
+df_mod_for_gender = df %>% 
+  tidyr::gather( `EPA_0`: `MCTR2_8.8`, key = "meta", value = "meta_reading")  %>% 
+  tidyr::nest(-meta) %>% 
+  mutate( mod = purrr::map(.$data, gender_model)) %>% 
+  mutate( mod_tidy= purrr::map(.$mod, broom::tidy)) %>% 
+  select( meta, mod_tidy) %>% 
+  tidyr::unnest() %>% 
+  filter( term =="gender") %>% 
+  select(meta, p.value) %>% 
+  mutate( response = "gender") %>% 
+  arrange(p.value)
+
+
+df_gender_race = dplyr::bind_rows(df_mod_for_race, df_mod_for_gender)
+df_gender_race = df_gender_race %>% 
+  rename( term = meta) %>% 
+  mutate(p.value = -1 * log10(p.value)) %>% 
+  mutate( estimate = as.numeric(rescale(p.value))) %>% 
+  dplyr::mutate(estimate = round(estimate,2))
+
+
+
+
+
+## get p and cor from spearmn cor
+correlation = psych::corr.test( df_non_case_s[-1], use = "complete.obs", method = "spearman")
+
+cor_df = as.data.frame(correlation$r)
+p_df = as.data.frame(correlation$p)
+
+cor_df_long = cor_df %>% 
+  tibble::rownames_to_column( var = "SPM1") %>% 
+  gather(EPA_0:MCTR2_8.8, key="SPM2", value = "SPM_cor" ) %>% 
+  filter( SPM1 != SPM2) %>% 
+  filter( SPM1 %in% c("EPA_0","AA_0.1", "DHA_0", "LTB4_0", "TXB2_24.9"))
+
+p_df_long = p_df %>% 
+  tibble::rownames_to_column( var = "SPM1") %>% 
+  gather(EPA_0:MCTR2_8.8, key="SPM2", value = "SPM_p" ) %>% 
+  filter( SPM1 != SPM2) %>% 
+  filter( SPM1 %in% c("EPA_0","AA_0.1", "DHA_0", "LTB4_0", "TXB2_24.9"))
+
+
+plot_data = cor_df_long %>% 
+  left_join( p_df_long, by = c("SPM1", "SPM2")) %>% 
+  dplyr::mutate(SPM_cor = round(SPM_cor,2))
+names(plot_data) = c("response", "term",     "estimate", "p.value" )
