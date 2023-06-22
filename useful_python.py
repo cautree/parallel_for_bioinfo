a478b286eb5403cc8fee9772aaf5f2110494c559
Yanyan Liu <yanyanliujs@gmail.com>
2023-06-22 16:45:28 -0400

    Update useful_python.py

--- useful_python.py
+++ useful_python.py
@@ -272,3 +272,20 @@ newTeam_1 = {k: v  for k, v in team1.items()}
 newTeam_2 = {k: v  for k, v in team2.items()}
 new = newTeam_1 | newTeam_2
 print(new)
+
+
+
+
+# define a list of temperature data points
+ctemps = [5, 10, 12, 14, 10, 23, 41, 30, 12, 24, 12, 18, 29]
+
+# build a set of unique Fahrenheit temperatures
+ftemps1 = [(t * 9/5) + 32 for t in ctemps]
+print(ftemps1)
+ftemps2 = {(t * 9/5) + 32 for t in ctemps}
+print(ftemps2)
+
+# build a set from an input source
+s_temp = "The quick brown fox jumped over the lazy dog"
+chars = {c.upper() for c in s_temp if not c.isspace()}
+print(chars)
