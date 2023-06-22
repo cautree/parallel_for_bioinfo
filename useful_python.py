e9f64d7e25b342fba08b3c147494aa63ddef3028
Yanyan Liu <yanyanliujs@gmail.com>
2023-06-22 16:59:25 -0400

    Update useful_python.py

--- useful_python.py
+++ useful_python.py
@@ -289,3 +289,34 @@ print(ftemps2)
 s_temp = "The quick brown fox jumped over the lazy dog"
 chars = {c.upper() for c in s_temp if not c.isspace()}
 print(chars)
+
+
+
+import string
+import pprint
+
+
+test_str = "2 apples, 9 oranges?, 4 pears, Mike's 1 egg, Jane's 2 kiwis, $50!"
+
+# get the total length of the string
+l = len(test_str)
+
+# count the number characters
+nums = len([c for c in test_str if c.isnumeric()])
+
+# count the punctuation characters
+punct = len([c for c in test_str if c in string.punctuation])
+
+# use a set to count the unique letters
+unique = "".join({c for c in test_str if c.isalpha()})
+
+# print the data
+str_data = {
+    "Length:": l,
+    "Digits:": nums,
+    "Punctuation": punct,
+    "Unique Letters": unique,
+    "Unique Count": len(unique)
+}
+pprint.pp(str_data)
+print(str_data)
