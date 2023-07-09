f64609ab3fb79b0cc8d96b02b06b6a8525da1155
Yanyan Liu <yanyanliujs@gmail.com>
2023-07-08 22:38:59 -0400

    Update pandas_notes.py
    glob

--- pandas_notes.py
+++ pandas_notes.py
@@ -12,3 +12,15 @@ df_for_avg["avg2"] = df_for_avg.apply( lambda x: np.mean([x.math , x.physics, x.
 df_for_avg["avg3"] = [(x+y+z)/3 for x,y,z in  zip(df_for_avg.math, df_for_avg.physics, df_for_avg.chemistry)  ]
 
 df_for_avg["avg4"] = [get_mean(x,y,z) for x,y,z in  zip(df_for_avg.math, df_for_avg.physics, df_for_avg.chemistry)  ]
+
+
+
+import pathlib2 as pl2
+ps = pl2.Path('data/sp3')
+
+dfs = (
+    pd.read_csv(p, encoding='utf8') for p in ps.glob('*.csv')
+)
+
+res = pd.concat(dfs)
+res
