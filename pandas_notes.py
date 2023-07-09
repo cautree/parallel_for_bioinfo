76b13feacb9b18c10c6e3004c6ab3224c6fa31e4
Yanyan Liu <yanyanliujs@gmail.com>
2023-07-09 08:10:42 -0400

    Update pandas_notes.py

--- pandas_notes.py
+++ pandas_notes.py
@@ -3,7 +3,7 @@ import numpy as np
 def get_mean(x,y,z):
     m = np.mean([x,y,z])
     return(m)
-  
+##four different ways to create a new columns using three columns in the data  
 df_for_avg["avg"] = df_for_avg.apply( lambda x: (x.math + x.physics+ x.chemistry)/3, axis =1)
 
 df_for_avg["avg2"] = df_for_avg.apply( lambda x: np.mean([x.math , x.physics, x.chemistry]), axis =1)
@@ -24,3 +24,20 @@ dfs = (
 
 res = pd.concat(dfs)
 res
+
+
+## create two new columns using one current column
+df_test["first_last"] = df_test.apply( lambda x: x.Name.split(', '), axis =1 )
+df_test[['first', 'last']] = pd.DataFrame(df_test["first_last"].tolist(), index=df_test.index)
+
+
+
+
+
+
+
+
+
+
+
+
